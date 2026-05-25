829d2471172cc4fa395db895f706cbb383b82947
Revaldi <revaldiumr12@gmail.com>
2026-05-25 09:55:08 +0700

    fix: main and question model

--- lib/data/models/question_model.g.dart
+++ lib/data/models/question_model.g.dart
@@ -31,14 +31,13 @@ class QuestionModelAdapter extends TypeAdapter<QuestionModel> {
       solveCount: fields[11] as int,
       createdAt: fields[12] as DateTime,
       updatedAt: fields[13] as DateTime,
-      imageUrl: fields[14] as String?,
     );
   }
 
   @override
   void write(BinaryWriter writer, QuestionModel obj) {
     writer
-      ..writeByte(15)
+      ..writeByte(14)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
@@ -66,9 +65,7 @@ class QuestionModelAdapter extends TypeAdapter<QuestionModel> {
       ..writeByte(12)
       ..write(obj.createdAt)
       ..writeByte(13)
-      ..write(obj.updatedAt)
-      ..writeByte(14)
-      ..write(obj.imageUrl);
+      ..write(obj.updatedAt);
   }
 
   @override
